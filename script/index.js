a8fcfd14392b88f80666162610bd5e297eae1e66
kimcho808 <yooa2468@gmail.com>
2026-07-16 10:45:50 +0900

    탄생화 앱 문제 풀이

--- script/index.js
+++ script/index.js
@@ -1,20 +1,23 @@
+// 팝업창과 배경 변수로 만들기
 const popup = document.querySelector ('.popup');
 const popup_back = document.querySelector ('#popup_bg');
 console.log(popup);
 console.log(popup_back);
+// 자바스크립트에서 css 쓰려면 작성하는 속성 -> 객체.style.속성 = 값
 popup.style.display = 'none';
 popup_back.style.display = 'none';
-function popupShow() {
-    popup.style.display = 'flex';
-    popup_back.style.display = 'block';
-}
+// 팝업창과 배경 숨기기
 function popupHide() {
     popup.style.display = 'none';
     popup_back.style.display = 'none';
 }
-let month;
-month = '12';
-let monthNum = document.querySelector ('#num');
+// 팝업창과 배경 띄우기
+function popupShow() {
+    popup.style.display = 'flex';
+    popup_back.style.display = 'block';
+}
+// input '12' 값으로 바꾸기
+const monthNum = document.querySelector ('#num');
 function clickNum() {
     monthNum.value = '12'
 }
