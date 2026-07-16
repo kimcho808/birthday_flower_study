dea3002a99b4243fa848a761778cd8c6f9076b87
kimcho808 <yooa2468@gmail.com>
2026-07-16 10:57:45 +0900

    탄생화 앱 문제풀이

--- script/index.js
+++ script/index.js
@@ -12,6 +12,10 @@ function popupHide() {
     popup_back.style.display = 'none';
 }
 // 팝업창과 배경 띄우기
+// 자바스크립트에서 display=non으로 숨겼다가 특정이벤트에 따라 다시 보이고 싶을 때
+// 무조건 block을 쓰는게 아닌 그 선택자가 원래 가지고 있는 display속성으로 되돌리기
+// display:flex가 선택자에 원래 있었다면 자바스크립트도 display=flex
+// display가 선택자에 없었다면? 원래 태그 고유속성에 따라 display=block또는 display=inline
 function popupShow() {
     popup.style.display = 'flex';
     popup_back.style.display = 'block';
